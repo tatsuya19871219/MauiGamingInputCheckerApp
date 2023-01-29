1e3ea962d1a403cd63aa22430581bd9e62778b6e
Tatsuya Yamada <tatsuya19871219@gmail.com>
2023-01-29 15:44:01 +0900

    Create a custom view for gaming input.

---
+++ MauiGamingInputCheckerApp/CustomViews/GamingInputView.xaml.cs
@@ -0,0 +1,43 @@
+using MauiGamingInputCheckerApp.Models;
+using System.Net;
+
+namespace MauiGamingInputCheckerApp.CustomViews;
+
+public partial class GamingInputView : ContentView
+{
+	public GamingInputView()
+	{
+		InitializeComponent();
+	}
+
+	
+	public void KeyDown(GamingInputArgs args)
+	{
+        switch (args.Keys)
+        {
+            case GamingInput.KEYS.KEY_UP: AnimateButton(KEY_UP); break;
+            case GamingInput.KEYS.KEY_DOWN: AnimateButton(KEY_DOWN); break;
+            case GamingInput.KEYS.KEY_LEFT: AnimateButton(KEY_LEFT); break;
+            case GamingInput.KEYS.KEY_RIGHT: AnimateButton(KEY_RIGHT); break;
+
+            case GamingInput.KEYS.KEY_Y: AnimateButton(KEY_1); break;
+            case GamingInput.KEYS.KEY_A: AnimateButton(KEY_2); break;
+            case GamingInput.KEYS.KEY_B: AnimateButton(KEY_3); break;
+            case GamingInput.KEYS.KEY_X: AnimateButton(KEY_4); break;
+        }
+    }
+
+
+    async void AnimateButton(VisualElement button)
+    {
+        double initialScale = button.Scale;
+
+        double activeScale = 1.5 * initialScale;
+
+        // animation
+        await button.ScaleTo(activeScale, 100);
+        await Task.Delay(50); // Android needs this, but why?
+        await button.ScaleTo(initialScale, 150);
+
+    }
+}
--- MauiGamingInputCheckerApp/MainPage.xaml.cs
+++ MauiGamingInputCheckerApp/MainPage.xaml.cs
@@ -57,32 +57,12 @@ public partial class MainPage : ContentPage
 
 	void KeyDownEvent(GamingInputArgs args)
 	{
-		switch(args.Keys)
-		{
-            case GamingInput.KEYS.KEY_UP: AnimateButton(KEY_UP); break;
-			case GamingInput.KEYS.KEY_DOWN: AnimateButton(KEY_DOWN); break;
-			case GamingInput.KEYS.KEY_LEFT: AnimateButton(KEY_LEFT); break;
-			case GamingInput.KEYS.KEY_RIGHT: AnimateButton(KEY_RIGHT); break;
 
-            case GamingInput.KEYS.KEY_Y: AnimateButton(KEY_1); break;
-            case GamingInput.KEYS.KEY_A: AnimateButton(KEY_2); break;
-            case GamingInput.KEYS.KEY_B: AnimateButton(KEY_3); break;
-            case GamingInput.KEYS.KEY_X: AnimateButton(KEY_4); break;
-        }
+		MyInput.KeyDown(args);
+
     }
 
-	async void AnimateButton(VisualElement button)
-	{
-		double initialScale = button.Scale;
 
-		double activeScale = 1.5 * initialScale;
-
-		// animation
-		await button.ScaleTo(activeScale, 100);
-		await Task.Delay(50); // Android needs this, but why?
-		await button.ScaleTo(initialScale, 150);
-
-	}
 
 
     //
@@ -91,18 +71,18 @@ public partial class MainPage : ContentPage
 		double density = DeviceDisplay.Current.MainDisplayInfo.Density;
 
 		// resize buttons
-		foreach(VisualElement button in ControllerGUIButtons.Children)
-		{
-			button.Scale /= density;
-			button.TranslationX /= density;
-			button.TranslationY /= density;
-		}
+		//foreach(VisualElement button in ControllerGUIButtons.Children)
+		//{
+		//	button.Scale /= density;
+		//	button.TranslationX /= density;
+		//	button.TranslationY /= density;
+		//}
 
-#if WINDOWS
-		ControllerGUIContainer.Scale = 1;
-#elif ANDROID
-		ControllerGUIContainer.Scale = 2;
-#endif
+//#if WINDOWS
+//		ControllerGUIContainer.Scale = 1;
+//#elif ANDROID
+//		ControllerGUIContainer.Scale = 2;
+//#endif
 
 		base.OnSizeAllocated(width, height);
     }
